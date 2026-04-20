afed1ff884a78502747dbb03b56975044de7d2a6
awb99 <awb99@gmail.com>
2026-04-20 13:12:18 -0500

    splits

--- src/quanta/notebook/import_task/eodhd_splits.clj
+++ src/quanta/notebook/import_task/eodhd_splits.clj
@@ -21,30 +21,17 @@
  (m/? (import-splits-list
        ctx
        (assoc base :list "etf-10mio"))))
-
-; {:asset "XLY", :start #time/date "1980-01-01", :end #time/date "2026-04-19"}
-; "Elapsed time: 58455.765072 msecs"
-;{:success 947, :error 0, :error-details []}
-; 530 splits
 ; "Elapsed time: 131024.045292 msecs"
-{:success 1908, :error 0, :error-details []}
+; {:success 1908, :error 0, :error-details []}
 
 (time
  (m/? (import-splits-list
        ctx
        (assoc base :list "equity-20mio"))))
-; "Elapsed time: 115999.964061 msecs"
-{:success 1000, :error 0, :error-details []}
 ; "Elapsed time: 195041.628529 msecs"
 {:success 1908, :error 0, :error-details []}
-; "Elapsed time: 342120.966022 msecs"
-{:success 4019, :error 0, :error-details []}
-
-{:asset "JNJ", :start #time/date "1980-01-01", :end #time/date "2026-04-19"}
 
 (m/? (get-splits (:ss system) "IGE"))
 (m/? (get-splits (:ss system) "MSFT"))
-
 (m/? (get-splits (:ss system) "SLV"))
-
 (m/? (delete-splits (:ss system) "SLV"))
